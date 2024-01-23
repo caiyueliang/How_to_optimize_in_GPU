41ccf2706cbf4a39d1e7c2cd089957e2e450e99b
caiyueliang <393900414@qq.com>
2024-01-23 22:51:15 +0800

    update

--- reduce/reduce_v0_baseline_cyl.cu
+++ reduce/reduce_v0_baseline_cyl.cu
@@ -8,7 +8,8 @@
 #define THREAD_PER_BLOCK 256
 
 __global__ void reduce0(float*vec_in, float*vec_out) {
-    __shared__ float* shared_vec = THREAD_PER_BLOCK * sizeof(float);
+    //__shared__ float* shared_vec = THREAD_PER_BLOCK * sizeof(float);
+    __shared__ float shared_vec[THREAD_PER_BLOCK];
 
     int id = threadIdx.x;
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
