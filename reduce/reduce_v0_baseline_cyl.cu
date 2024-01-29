da03931f6b8090346db06bb53811fa2248e14c25
caiyueliang <393900414@qq.com>
2024-01-29 15:48:03 +0800

    update

--- reduce/reduce_v0_baseline_cyl.cu
+++ reduce/reduce_v0_baseline_cyl.cu
@@ -7,6 +7,7 @@
 #include <iostream>
 
 #define THREAD_PER_BLOCK 256
+#define WARP_SIZE 32
 
 // ===================================================================================================
 // reduce0版本：baseline
@@ -150,15 +151,6 @@ __global__ void reduce3(float *d_in, float *d_out) {
 
 // ===================================================================================================
 // reduce_v6版本:
-template <typename T>
-T warpReduceSum(T val) {
-    // T local_var = xxx;
-    for(int mask = 16; mask > 0; mask >>= 1) {
-        val += __shfl_xor_sync(uint(-1), val, mask, 32);
-    }
-    return val;
-}
-
 template <unsigned int blockSize>
 __device__ void warpReduce(volatile float* cache, unsigned int tid){
     if (blockSize >= 64)cache[tid]+=cache[tid+32];
@@ -206,6 +198,49 @@ __global__ void reduce6(float*vec_in, float*vec_out) {
         vec_out[blockIdx.x] = shared_vec[0];
     }
 }
+
+// ===================================================================================================
+// reduce_v7 版本:
+template <unsigned int blockSize>
+__device__ __forceinline__ float warpReduceSum(float sum) {
+    if (blockSize >= 32)sum += __shfl_down_sync(0xffffffff, sum, 16); // 0-16, 1-17, 2-18, etc.
+    if (blockSize >= 16)sum += __shfl_down_sync(0xffffffff, sum, 8);// 0-8, 1-9, 2-10, etc.
+    if (blockSize >= 8)sum += __shfl_down_sync(0xffffffff, sum, 4);// 0-4, 1-5, 2-6, etc.
+    if (blockSize >= 4)sum += __shfl_down_sync(0xffffffff, sum, 2);// 0-2, 1-3, 4-6, 5-7, etc.
+    if (blockSize >= 2)sum += __shfl_down_sync(0xffffffff, sum, 1);// 0-1, 2-3, 4-5, etc.
+    return sum;
+}
+
+template <unsigned int blockSize, int NUM_PER_THREAD>
+__global__ void reduce7(float *d_in,float *d_out, unsigned int n){
+    float sum = 0;
+
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * (blockSize * NUM_PER_THREAD) + threadIdx.x;
+
+    #pragma unroll
+    for(int iter=0; iter<NUM_PER_THREAD; iter++){
+        sum += d_in[i+iter*blockSize];
+    }
+    
+    // Shared mem for partial sums (one per warp in the block)
+    static __shared__ float warpLevelSums[WARP_SIZE]; 
+    const int laneId = threadIdx.x % WARP_SIZE;
+    const int warpId = threadIdx.x / WARP_SIZE;
+
+    sum = warpReduceSum<blockSize>(sum);
+
+    if(laneId == 0 )warpLevelSums[warpId] = sum;
+    __syncthreads();
+    // read from shared memory only if that warp existed
+    sum = (threadIdx.x < blockDim.x / WARP_SIZE) ? warpLevelSums[laneId] : 0;
+    // Final reduce using first warp
+    if (warpId == 0) sum = warpReduceSum<blockSize/WARP_SIZE>(sum); 
+    // write result for this block to global mem
+    if (tid == 0) d_out[blockIdx.x] = sum;
+}
+
 
 // ===================================================================================================
 bool check(float *out, float *res, int n) {
@@ -280,6 +315,11 @@ int main(int argc, char **argv) {
         reduce3<<<Grid, Block, block_size*sizeof(float)>>>(d_a, d_out);
     } else if (version == 6) {   
         reduce6<<<Grid, Block, block_size*sizeof(float)>>>(d_a, d_out);
+    } else if (version == 7) {   
+        // reduce7<><<<Grid, Block, block_size*sizeof(float)>>>(d_a, d_out);
+        // const int NUM_PER_BLOCK = N / block_num;
+        const int NUM_PER_THREAD = N / block_num / block_size;
+        reduce7<block_size, NUM_PER_THREAD><<<Grid,Block>>>(d_a, d_out, N);
     } else {
         reduce0<<<Grid, Block, block_size*sizeof(float)>>>(d_a, d_out);
         reduce1<<<Grid, Block, block_size*sizeof(float)>>>(d_a, d_out);
